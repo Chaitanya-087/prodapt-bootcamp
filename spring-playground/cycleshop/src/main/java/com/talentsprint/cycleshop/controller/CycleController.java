1beb9ccd06c6d2bb5c579cdcf8801d4b6f0d4ba5
chaitanya <saisrikrishna.j@prodapt.com>
2023-09-05 17:00:44 +0530

    tinkering

--- spring-playground/cycleshop/src/main/java/com/talentsprint/cycleshop/controller/CycleController.java
+++ spring-playground/cycleshop/src/main/java/com/talentsprint/cycleshop/controller/CycleController.java
@@ -1,6 +1,7 @@
 package com.talentsprint.cycleshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
